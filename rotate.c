123538f999742de6f63bb5436cc3261cc32d23af
Arinolaoluwa Makanjuola <arimakanjuola@my.unt.edu>
2026-08-26 19:33:14 -0500

    Create rotate.c

---
+++ rotate.c
@@ -0,0 +1,17 @@
+#include "major1.h"
+
+// Gloria Ukattah, CSCE 3600, 3/6/2026
+// This program uses a 32-bit integer to do a bitwise rotate-right operation.
+
+unsigned int rotate_right(unsigned int num, int positions) {
+    unsigned int result;
+
+    if (positions == 0) {
+        result = num;
+    } else {
+        result = (num >> positions) | (num << (32 - positions));
+    }
+
+    return result;
+
+}
